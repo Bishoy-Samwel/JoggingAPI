b5bdf2bdcfd5ce21b1e8d15a7a89470c6f3c85eb
Bishoy <bishoy.samwuel@gmail.com>
2022-10-05 19:37:31 +0200

    refactor: Configure  User model
    jwt_revocation_strategy: JwtDenylist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, :registerable,
+         jwt_revocation_strategy: JwtDenylist
 end
