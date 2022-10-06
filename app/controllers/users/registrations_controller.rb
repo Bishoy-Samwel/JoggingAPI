d543e1d231c82a51e79c8ffbe6bc3de6f6a8edc0
Bishoy <bishoy.samwuel@gmail.com>
2022-10-06 18:25:39 +0200

    build: show errors when the register fail

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,7 +18,7 @@ module Users
     end
 
     def register_failed
-      render json: { message: 'Signed up failure.' }, status: :unprocessable_entity
+      render json: { message: "Signed up failure.", errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
