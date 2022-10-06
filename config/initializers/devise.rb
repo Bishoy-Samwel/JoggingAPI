1df403475fd8467308a8fbaecfd535afa0d29f0d
Bishoy <bishoy.samwuel@gmail.com>
2022-10-06 21:06:45 +0200

    refactor: change the dispatch revocation endpoints

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,10 +13,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      ['POST', %r{^/sign_in$}]
+      ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/sign_out$}]
+      ['DELETE', %r{^/logout$}]
     ]
     jwt.expiration_time = 15.day.to_i
   end
