bdc57d773cab7be78d592abd367ba2463c6b078b
Bishoy <bishoy.samwuel@gmail.com>
2022-10-08 21:08:22 +0200

    refactor: add a validation user must have a role on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  rolify
   has_many :jogging_times
   after_create :assign_default_role
 
-  def assign_default_role
-    self.add_role(:regular) if self.roles.blank?
-  end
-  rolify
+  validate :must_have_a_role, on: :update
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
@@ -17,4 +17,15 @@ class User < ApplicationRecord
          :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+  private
+  def assign_default_role
+    self.add_role(:regular) if self.roles.blank?
+  end
+
+  def must_have_a_role
+    unless role.any?
+      errors.add(:roles, 'Must at least have 1 rule')
+    end
+  end
+
 end
