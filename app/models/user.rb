4a2d52d139ad85223d6a39b706de955b23a85bf2
Bishoy <bishoy.samwuel@gmail.com>
2022-10-08 20:42:29 +0200

    refactor: add default role regular to new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :jogging_times
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:regular) if self.roles.blank?
+  end
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
