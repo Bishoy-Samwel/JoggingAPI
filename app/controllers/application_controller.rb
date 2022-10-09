e84948b8fb6a5a38ee5ee3cfd607b2d3de6c45b7
Bishoy <bishoy.samwuel@gmail.com>
2022-10-09 19:28:28 +0200

    refactor: overide notAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   skip_before_action :verify_authenticity_token,  raise: false
-  include Pundtit
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render :json => { :error => 'Unotharized request' }, :status => 401
+  end
 end
