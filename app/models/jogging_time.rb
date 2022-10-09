b4a7025b96ab660166cce3a42f9123c987acfa61
Bishoy <bishoy.samwuel@gmail.com>
2022-10-09 21:38:47 +0200

    build: add creators association to JoggingTIme

--- app/models/jogging_time.rb
+++ app/models/jogging_time.rb
@@ -1,5 +1,5 @@
 class JoggingTime < ApplicationRecord
   # belongs_to :user
   resourcify
-  # has_many :creators, -> { where(:roles => {name: :creator}) }, through: :roles, class_name: 'User', source: :users
+  has_many :creators, -> { where(:roles => {name: :creator}) }, through: :roles, class_name: 'User', source: :users
 end
