999ebf7f5392806c8798ccf2f0a887b40cbe476f
Bishoy <bishoy.samwuel@gmail.com>
2022-10-09 17:22:21 +0200

    refactor: resourcify jogging times

--- app/models/jogging_time.rb
+++ app/models/jogging_time.rb
@@ -1,3 +1,5 @@
 class JoggingTime < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  resourcify
+  # has_many :creators, -> { where(:roles => {name: :creator}) }, through: :roles, class_name: 'User', source: :users
 end
