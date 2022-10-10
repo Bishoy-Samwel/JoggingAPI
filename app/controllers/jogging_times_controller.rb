74d6cf31ae52d1e5c3a46b21e55566d14a0bad67
Bishoy <bishoy.samwuel@gmail.com>
2022-10-10 12:16:09 +0200

    refactor: add  policy_scope to joggingtimes

--- app/controllers/jogging_times_controller.rb
+++ app/controllers/jogging_times_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class JoggingTimesController < ApplicationController
-  before_action :set_jogging_time, only: %i[ show update destroy ]
+  before_action :set_jogging_time, only: %i[show update destroy]
   before_action :authenticate_user!
 
   # GET /jogging_times
   def index
-    @jogging_times = JoggingTime.all
+    @jogging_times = policy_scope(JoggingTime).all
 
     render json: @jogging_times
   end
@@ -41,13 +43,18 @@ class JoggingTimesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_jogging_time
-      @jogging_time = JoggingTime.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def jogging_time_params
-      params.fetch(:jogging_time, {})
+  # Use callbacks to share common setup or constraints between actions.
+  def set_jogging_time
+    if policy_scope(JoggingTime).find(params[:id]).present?
+      @jogging_time = policy_scope(JoggingTime).find(params[:id])
+    else
+      render json: { error: 'Unotharized request' }, status: 401
     end
+  end
+
+  # Only allow a list of trusted parameters through.
+  def jogging_time_params
+    params.fetch(:jogging_time, {})
+  end
 end
